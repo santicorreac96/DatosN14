6248a405c9cb3680a4fa4afe93768367ff7e4a35
SANTIAGO <santicorreac96@hotmail.com>
2014-08-30 13:44:57 -0500

    Nueva clase

--- N14/src/estructuraDeDatos/TablaHash.java
+++ N14/src/estructuraDeDatos/TablaHash.java
@@ -1,5 +1,30 @@
 package estructuraDeDatos;
 
-public class TablaHash {
+public class TablaHash<E,K> implements ITablaHash<E, K>
+{
+
+	@Override
+	public E agregar(E nElemento, K nLlave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E darElemento(K pLlave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E eliminar(K pLlave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int darCantidadElementos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
