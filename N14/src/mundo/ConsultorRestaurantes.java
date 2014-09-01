c982aa005f20de69e5f1259f24482aa1cfd05273
SANTIAGO <santicorreac96@hotmail.com>
2014-09-01 14:46:56 -0500

    Datos

--- N14/src/mundo/ConsultorRestaurantes.java
+++ N14/src/mundo/ConsultorRestaurantes.java
@@ -1,7 +1,23 @@
 package mundo;
 
-public class ConsultorRestaurantes 
+import java.io.File;
+
+public class ConsultorRestaurantes implements IConsultorRestaurantes
 {
+
+	@Override
+	public void cargarXLS() 
+	{
+		try 
+		{
+			File libroActual =  new File("./data/)
+		}
+		catch ( Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}
 	
 	
 
